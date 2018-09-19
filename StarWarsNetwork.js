14ea45cc1db4499ec299219b48188e1eae452c42
prestondunton <38969742+prestondunton@users.noreply.github.com>
2018-09-19 13:32:10 -0600

    Add files via upload

---
+++ StarWarsNetwork.js
@@ -0,0 +1,438 @@
+var starwarsNetwork = {
+  "nodes": [
+    {
+      "id" : 0,
+      "name": "R2-D2",
+      "value": 40,
+      "colour": "#bde0f6"
+    },
+    {
+      "id" : 1,
+      "name": "CHEWBACCA",
+      "value": 30,
+      "colour": "#A0522D"
+    },
+    {
+      "id" : 2,
+      "name": "C-3PO",
+      "value": 43,
+      "colour": "#FFD700"
+    },
+    {
+      "id" : 3,
+      "name": "LUKE",
+      "value": 89,
+      "colour": "#3881e5"
+    },
+    {
+      "id" : 4,
+      "name": "DARTH VADER",
+      "value": 26,
+      "colour": "#000000"
+    },
+    {
+      "id" : 5,
+      "name": "CAMIE",
+      "value": 4,
+      "colour": "#808080"
+    },
+    {
+      "id" : 6,
+      "name": "BIGGS",
+      "value": 19,
+      "colour": "#808080"
+    },
+    {
+    "id" : 7,
+      "name": "LEIA",
+      "value": 26,
+      "colour": "#DCDCDC"
+    },
+    {
+      "id" : 8,
+      "name": "BERU",
+      "value": 6,
+      "colour": "#808080"
+    },
+    {
+      "id" : 9,
+      "name": "OWEN",
+      "value": 7,
+      "colour": "#808080"
+    },
+    {
+      "id" : 10,
+      "name": "OBI-WAN",
+      "value": 23,
+      "colour": "#48D1CC"
+    },
+    {
+      "id" : 11,
+      "name": "MOTTI",
+      "value": 4,
+      "colour": "#808080"
+    },
+    {
+      "id" : 12,
+      "name": "TARKIN",
+      "value": 13,
+      "colour": "#808080"
+    },
+    {
+      "id" : 13,
+      "name": "HAN",
+      "value": 45,
+      "colour": "#ff9400"
+    },
+    {
+      "id" : 14,
+      "name": "GREEDO",
+      "value": 3,
+      "colour": "#808080"
+    },
+    {
+      "id" : 15,
+      "name": "JABBA",
+      "value": 3,
+      "colour": "#808080"
+    },
+    {
+      "id" : 16,
+      "name": "DODONNA",
+      "value": 4,
+      "colour": "#808080"
+    },
+    {
+      "id" : 17,
+      "name": "GOLD LEADER",
+      "value": 15,
+      "colour": "#808080"
+    },
+    {
+      "id" : 18,
+      "name": "WEDGE",
+      "value": 16,
+      "colour": "#808080"
+    },
+    {
+      "id" : 19,
+      "name": "RED LEADER",
+      "value": 34,
+      "colour": "#808080"
+    },
+    {
+      "id" : 20,
+      "name": "RED TEN",
+      "value": 9,
+      "colour": "#808080"
+    },
+    {
+      "id" : 21,
+      "name": "GOLD FIVE",
+      "value": 9,
+      "colour": "#808080"
+    }
+  ],
+  "links": [
+    {
+      "source": 1,
+      "target": 0,
+      "weight": 3
+    },
+    {
+      "source": 2,
+      "target": 0,
+      "weight": 17
+    },
+    {
+      "source": 8,
+      "target": 0,
+      "weight": 1
+    },
+    {
+      "source": 3,
+      "target": 0,
+      "weight": 14
+    },
+    {
+      "source": 9,
+      "target": 0,
+      "weight": 1
+    },
+    {
+      "source": 10,
+      "target": 0,
+      "weight": 4
+    },
+    {
+      "source": 7,
+      "target": 0,
+      "weight": 5
+    },
+    {
+      "source": 6,
+      "target": 0,
+      "weight": 1
+    },
+    {
+      "source": 13,
+      "target": 0,
+      "weight": 6
+    },
+    {
+      "source": 1,
+      "target": 10,
+      "weight": 4
+    },
+    {
+      "source": 2,
+      "target": 1,
+      "weight": 4
+    },
+    {
+      "source": 1,
+      "target": 3,
+      "weight": 14
+    },
+    {
+      "source": 1,
+      "target": 13,
+      "weight": 19
+    },
+    {
+      "source": 1,
+      "target": 7,
+      "weight": 8
+    },
+    {
+      "source": 5,
+      "target": 3,
+      "weight": 2
+    },
+    {
+      "source": 6,
+      "target": 5,
+      "weight": 2
+    },
+    {
+      "source": 6,
+      "target": 3,
+      "weight": 4
+    },
+    {
+      "source": 4,
+      "target": 7,
+      "weight": 1
+    },
+    {
+      "source": 8,
+      "target": 3,
+      "weight": 3
+    },
+    {
+      "source": 8,
+      "target": 9,
+      "weight": 3
+    },
+    {
+      "source": 8,
+      "target": 2,
+      "weight": 2
+    },
+    {
+      "source": 3,
+      "target": 9,
+      "weight": 3
+    },
+    {
+      "source": 2,
+      "target": 3,
+      "weight": 18
+    },
+    {
+      "source": 2,
+      "target": 9,
+      "weight": 2
+    },
+    {
+      "source": 2,
+      "target": 7,
+      "weight": 6
+    },
+    {
+      "source": 7,
+      "target": 3,
+      "weight": 17
+    },
+    {
+      "source": 8,
+      "target": 7,
+      "weight": 1
+    },
+    {
+      "source": 3,
+      "target": 10,
+      "weight": 19
+    },
+    {
+      "source": 2,
+      "target": 10,
+      "weight": 6
+    },
+    {
+      "source": 7,
+      "target": 10,
+      "weight": 1
+    },
+    {
+      "source": 11,
+      "target": 12,
+      "weight": 2
+    },
+    {
+      "source": 4,
+      "target": 11,
+      "weight": 1
+    },
+    {
+      "source": 4,
+      "target": 12,
+      "weight": 7
+    },
+    {
+      "source": 13,
+      "target": 10,
+      "weight": 9
+    },
+    {
+      "source": 13,
+      "target": 3,
+      "weight": 26
+    },
+    {
+      "source": 14,
+      "target": 13,
+      "weight": 1
+    },
+    {
+      "source": 13,
+      "target": 15,
+      "weight": 1
+    },
+    {
+      "source": 2,
+      "target": 13,
+      "weight": 6
+    },
+    {
+      "source": 7,
+      "target": 11,
+      "weight": 1
+    },
+    {
+      "source": 7,
+      "target": 12,
+      "weight": 1
+    },
+    {
+      "source": 13,
+      "target": 7,
+      "weight": 13
+    },
+    {
+      "source": 4,
+      "target": 10,
+      "weight": 1
+    },
+    {
+      "source": 16,
+      "target": 17,
+      "weight": 1
+    },
+    {
+      "source": 16,
+      "target": 18,
+      "weight": 1
+    },
+    {
+      "source": 16,
+      "target": 3,
+      "weight": 1
+    },
+    {
+      "source": 17,
+      "target": 18,
+      "weight": 1
+    },
+    {
+      "source": 17,
+      "target": 3,
+      "weight": 1
+    },
+    {
+      "source": 3,
+      "target": 18,
+      "weight": 2
+    },
+    {
+      "source": 6,
+      "target": 7,
+      "weight": 1
+    },
+    {
+      "source": 7,
+      "target": 19,
+      "weight": 1
+    },
+    {
+      "source": 3,
+      "target": 19,
+      "weight": 3
+    },
+    {
+      "source": 6,
+      "target": 19,
+      "weight": 3
+    },
+    {
+      "source": 6,
+      "target": 2,
+      "weight": 1
+    },
+    {
+      "source": 2,
+      "target": 19,
+      "weight": 1
+    },
+    {
+      "source": 19,
+      "target": 18,
+      "weight": 3
+    },
+    {
+      "source": 17,
+      "target": 19,
+      "weight": 1
+    },
+    {
+      "source": 6,
+      "target": 18,
+      "weight": 2
+    },
+    {
+      "source": 19,
+      "target": 20,
+      "weight": 1
+    },
+    {
+      "source": 6,
+      "target": 17,
+      "weight": 1
+    },
+    {
+      "source": 3,
+      "target": 20,
+      "weight": 1
+    }
+  ]
+};
